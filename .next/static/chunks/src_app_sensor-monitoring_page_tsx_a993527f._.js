28da94e5b99f90e03ba09117b624123687ea48f9
Sujith kumar E <sujithkrishnan1212@gmail.com>
2025-07-20 15:00:39 +0530

    Add .next/static/chunks/src_app_sensor-monitoring_page_tsx_a993527f._.js 1753003828390

---
+++ .next/static/chunks/src_app_sensor-monitoring_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_189af304._.js",
+  "static/chunks/src_62b13a8b._.js"
+],
+    source: "dynamic"
+});
